c3a32a44b0b1939dc87d8269d7304c8abbcd48e3
KPSBKarunagoda <136079049+KPSBKarunagoda@users.noreply.github.com>
2023-06-25 18:23:40 +0530

    Add files via upload

---
+++ Lab6-2023.06.23/question 6/Main class/Program.cs
@@ -0,0 +1,31 @@
+﻿using anotherclass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Class1 obj = new Class1();
+            int[] array = obj.CreateArray(size);
+            obj.TakeUserInputs(array);
+
+            Console.WriteLine("Values inside the array:");
+            foreach (int value in array)
+            {
+                Console.Write(value + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+    }
+
---
+++ Lab6-2023.06.23/question 6/Main class/another class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anotherclass
+
+{
+    internal class Class1
+    {
+        
+    public int[] CreateArray(int size)
+        {
+            int[] array = new int[size];
+            return array;
+        }
+
+        public void TakeUserInputs(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Enter a value: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+                array[i] = value;
+                Console.WriteLine("User input: {0}, 0", value);
+            }
+        }
+    }
+}
+
